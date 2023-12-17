87ebabe80ba5990d3811a3f8568809e5d7927581
Monicaular <>
2023-12-17 13:10:48 +0000

    Add title and show play rules function

--- run.py
+++ run.py
@@ -1,3 +1,39 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import words
+import random
+
+def show_title():
+    game_title = '''
+
+ .----------------.  .----------------.  .-----------------. .----------------.  .----------------.  .----------------.  .-----------------.
+| .--------------. || .--------------. || .--------------. || .--------------. || .--------------. || .--------------. || .--------------. |
+| |  ____  ____  | || |      __      | || | ____  _____  | || |    ______    | || | ____    ____ | || |      __      | || | ____  _____  | |
+| | |_   ||   _| | || |     /  \     | || ||_   \|_   _| | || |  .' ___  |   | || ||_   \  /   _|| || |     /  \     | || ||_   \|_   _| | |
+| |   | |__| |   | || |    / /\ \    | || |  |   \ | |   | || | / .'   \_|   | || |  |   \/   |  | || |    / /\ \    | || |  |   \ | |   | |
+| |   |  __  |   | || |   / ____ \   | || |  | |\ \| |   | || | | |    ____  | || |  | |\  /| |  | || |   / ____ \   | || |  | |\ \| |   | |
+| |  _| |  | |_  | || | _/ /    \ \_ | || | _| |_\   |_  | || | \ `.___]  _| | || | _| |_\/_| |_ | || | _/ /    \ \_ | || | _| |_\   |_  | |
+| | |____||____| | || ||____|  |____|| || ||_____|\____| | || |  `._____.'   | || ||_____||_____|| || ||____|  |____|| || ||_____|\____| | |
+| |              | || |              | || |              | || |              | || |              | || |              | || |              | |
+| '--------------' || '--------------' || '--------------' || '--------------' || '--------------' || '--------------' || '--------------' |
+ '----------------'  '----------------'  '----------------'  '----------------'  '----------------'  '----------------'  '----------------' 
+'''
+
+    print(game_title)
+    print("Welcome to Hangman Game!")
+
+def show_play_rules():
+    rules = [
+        "Insert your username so we know who are playing with.",
+        "Choose a difficulty level between easy, medium and hard.",
+        "Choose a letter that you believe it is in the word to guess.",
+        "A correct letter will show you its position(s) in the word. A wrong letter would add to the hangman.",
+        "You have six tries to guess, otherwise the man will be hanged",
+        "Let's begin"
+    ]
+    display_rules = input("Would you like to see the game rules? (y/n)" )
+    if display_rules.lower == 'n':
+        print("You have chosen to skip the rules, let's start the game")
+        insert_username()
+    else:
+        print(rules)
+
+
